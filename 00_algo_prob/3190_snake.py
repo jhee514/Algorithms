2afb3c9c19854b58fb2b47529878c6624d8c09db
J. Kim <514kim@gmail.com>
2020-02-05 23:43:19 +0900

    refactor

---
+++ 00_algo_prob/3190_snake.py
@@ -0,0 +1,26 @@
+import sys
+sys.stdin = open("3190_input.txt", "r")
+
+for tc in range(3):
+    n = int(input())
+    k = int(input())
+    apples = [list(map(int, input().split())) for _ in range(k)]
+    l = int(input())
+    dir = [list(input().split()) for _ in range(l)]
+
+
+    data = [[0]*n for _ in range(n)]
+    for apple in apples:
+        data[apple[0]][apple[1]] = 1
+
+    start = [0, 0]
+
+
+"""
+dir 앞에 있는 숫자 만큼 지나면 방향을 전환
+엘 => 왼쪽으로 90도 회전
+    좌 => 위 => 오 => 아래
+디 => 오른쪽으로 90도 회전
+    동 => 아래 => 왼쪽 => 위
+"""
+}
